8684aa4842d0761c36f2239800d16fc099ac1c84
abt576 <limut0913@gmail.com>
2024-05-07 00:15:38 +0900

    url error handling

--- Project/Vridge/src/main/java/com/gdsc/vridge/entity/VoiceEntity.java
+++ Project/Vridge/src/main/java/com/gdsc/vridge/entity/VoiceEntity.java
@@ -1,19 +1,14 @@
 package com.gdsc.vridge.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
 @Getter
 @Setter
 @AllArgsConstructor
 public class VoiceEntity {
 
-    @Id
     private String vid;
 
     private String name;
--- Project/Vridge/src/main/java/com/gdsc/vridge/service/TTSService.java
+++ Project/Vridge/src/main/java/com/gdsc/vridge/service/TTSService.java
@@ -19,7 +19,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
